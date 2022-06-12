c191e62690dbd37f5b9678ef2d2061ffbaee58e6
wurong <wurong@autoxing.com>
2022-06-12 21:01:31 +0800

    add ble5 ext adv scan demo.

--- advertise.c
+++ advertise.c
@@ -12,8 +12,27 @@
 #include <bluetooth/bluetooth.h>
 #include <bluetooth/hci.h>
 #include <bluetooth/hci_lib.h>
+#include "bt.h"
 
-struct hci_request ble_hci_request(uint16_t ocf, int clen, void * status, void * cparam)
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/time.h>
+#include <unistd.h>
+#include <signal.h>
+
+bool app_stopped = false;
+
+void sigint_handler(int sig)
+{
+	if (sig == SIGINT)
+	{
+		// ctrl+c退出时执行的代码
+		printf("will exit\n");
+		app_stopped = true;
+	}
+}
+
+struct hci_request ble_hci_request(uint16_t ocf, int clen, void *status, void *cparam)
 {
 	struct hci_request rq;
 	memset(&rq, 0, sizeof(rq));
@@ -26,94 +45,140 @@ struct hci_request ble_hci_request(uint16_t ocf, int clen, void * status, void *
 	return rq;
 }
 
-le_set_advertising_data_cp ble_hci_params_for_set_adv_data(char * name)
+struct bt_hci_cmd_le_set_ext_adv_data *ble_hci_params_for_set_adv_data(char *name)
 {
 	int name_len = strlen(name);
-
-	le_set_advertising_data_cp adv_data_cp;
-	memset(&adv_data_cp, 0, sizeof(adv_data_cp));
-
+	int data_len = 2 + name_len + 1;
+	struct bt_hci_cmd_le_set_ext_adv_data *adv_data_cp =
+		(struct bt_hci_cmd_le_set_ext_adv_data *)malloc(sizeof(struct bt_hci_cmd_le_set_ext_adv_data) + data_len);
+	memset(adv_data_cp, 0, sizeof(struct bt_hci_cmd_le_set_ext_adv_data) + data_len);
+	adv_data_cp->data_len = data_len;
+	adv_data_cp->handle = 0;
+	adv_data_cp->operation = 0x03;
+	adv_data_cp->fragment_preference = 0x01;
 	// Build simple advertisement data bundle according to:
-	// - ​"Core Specification Supplement (CSS) v5" 
+	// - ​"Core Specification Supplement (CSS) v5"
 	// ( https://www.bluetooth.org/en-us/specification/adopted-specifications )
 
-	adv_data_cp.data[0] = 0x02; // Length.
-	adv_data_cp.data[1] = 0x01; // Flags field.
-	adv_data_cp.data[2] = 0x01; // LE Limited Discoverable Flag set
-
-	adv_data_cp.data[3] = name_len + 1; // Length.
-	adv_data_cp.data[4] = 0x09; // Name field.
-	memcpy(adv_data_cp.data + 5, name, name_len);
-
-	adv_data_cp.length = strlen(adv_data_cp.data);
+	adv_data_cp->data[0] = 0x37;
+	adv_data_cp->data[1] = 0x09;
+	memcpy(adv_data_cp->data + 2, name, name_len);
+	adv_data_cp->data[2 + name_len] = 0x00; // end flag
 
 	return adv_data_cp;
 }
 
 int main()
 {
+	signal(SIGINT, sigint_handler);
 	int ret, status;
 
 	// Get HCI device.
 
 	const int device = hci_open_dev(hci_get_route(NULL));
-	if ( device < 0 ) { 
+	if (device < 0)
+	{
 		perror("Failed to open HC device.");
-		return 0; 
+		return 0;
 	}
+	printf("open device succuss.\n");
 
 	// Set BLE advertisement parameters.
-	
-	le_set_advertising_parameters_cp adv_params_cp;
-	memset(&adv_params_cp, 0, sizeof(adv_params_cp));
-	adv_params_cp.min_interval = htobs(0x0800);
-	adv_params_cp.max_interval = htobs(0x0800);
-	adv_params_cp.chan_map = 7;
-	
+
+	struct bt_hci_cmd_le_set_ext_adv_params ext_adv_params_cp;
+	memset(&ext_adv_params_cp, 0, sizeof(ext_adv_params_cp));
+	ext_adv_params_cp.handle = 0x00;
+	ext_adv_params_cp.min_interval[0] = 0x40;
+	ext_adv_params_cp.max_interval[0] = 0x40;
+	ext_adv_params_cp.evt_properties = 0x00;
+	ext_adv_params_cp.channel_map = 0x07; // all
+	ext_adv_params_cp.own_addr_type = 0x00;
+	ext_adv_params_cp.filter_policy = 0x00; // all accept
+	ext_adv_params_cp.primary_phy = 0x01;
+	ext_adv_params_cp.secondary_max_skip = 0;
+	ext_adv_params_cp.secondary_phy = 0x03;
+	ext_adv_params_cp.sid = 1;
+	ext_adv_params_cp.notif_enable = 0x00;
+
 	struct hci_request adv_params_rq = ble_hci_request(
-		OCF_LE_SET_ADVERTISING_PARAMETERS,
-		LE_SET_ADVERTISING_PARAMETERS_CP_SIZE, &status, &adv_params_cp);
-	
+		BT_HCI_CMD_LE_SET_EXT_ADV_PARAMS,
+		sizeof(struct bt_hci_cmd_le_set_ext_adv_params), &status, &ext_adv_params_cp);
+
 	ret = hci_send_req(device, &adv_params_rq, 1000);
-	if ( ret < 0 ) {
+	if (ret < 0)
+	{
 		hci_close_dev(device);
 		perror("Failed to set advertisement parameters data.");
 		return 0;
 	}
+	printf("set ext adv params succuss.\n");
 
 	// Set BLE advertisement data.
-	
-	le_set_advertising_data_cp adv_data_cp = ble_hci_params_for_set_adv_data("Intel Edison");
-	
-	struct hci_request adv_data_rq = ble_hci_request(
-		OCF_LE_SET_ADVERTISING_DATA,
-		LE_SET_ADVERTISING_DATA_CP_SIZE, &status, &adv_data_cp);
 
+	struct bt_hci_cmd_le_set_ext_adv_data *adv_data_cp = ble_hci_params_for_set_adv_data("Intel Edisonxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx11x1x2x3x4x5x6x7sedsagh");
+
+	struct hci_request adv_data_rq = ble_hci_request(
+		BT_HCI_CMD_LE_SET_EXT_ADV_DATA,
+		sizeof(struct bt_hci_cmd_le_set_ext_adv_data) + adv_data_cp->data_len, &status, adv_data_cp);
 	ret = hci_send_req(device, &adv_data_rq, 1000);
-	if ( ret < 0 ) {
+	if (ret < 0)
+	{
 		hci_close_dev(device);
 		perror("Failed to set advertising data.");
 		return 0;
 	}
+	printf("set ext adv data succuss.\n");
 
 	// Enable advertising.
 
-	le_set_advertise_enable_cp advertise_cp;
-	memset(&advertise_cp, 0, sizeof(advertise_cp));
-	advertise_cp.enable = 0x01;
+	struct bt_hci_cmd_le_set_ext_adv_enable *advertise_cp = (struct bt_hci_cmd_le_set_ext_adv_enable *)
+		malloc(sizeof(struct bt_hci_cmd_le_set_ext_adv_enable) + sizeof(struct bt_hci_cmd_ext_adv_set) * 1);
+	memset(advertise_cp, 0, sizeof(struct bt_hci_cmd_le_set_ext_adv_enable) + sizeof(struct bt_hci_cmd_ext_adv_set) * 1);
+	advertise_cp->enable = 0x01;
+	advertise_cp->num_of_sets = 1;
+	struct bt_hci_cmd_ext_adv_set *sets = (struct bt_hci_cmd_ext_adv_set *)advertise_cp->data;
+	sets[0].duration = 0;
+	sets[0].max_events = 0;
+	sets[0].handle = 0;
 
 	struct hci_request enable_adv_rq = ble_hci_request(
-		OCF_LE_SET_ADVERTISE_ENABLE,
-		LE_SET_ADVERTISE_ENABLE_CP_SIZE, &status, &advertise_cp);
+		BT_HCI_CMD_LE_SET_EXT_ADV_ENABLE,
+		sizeof(struct bt_hci_cmd_le_set_ext_adv_enable) + sizeof(struct bt_hci_cmd_ext_adv_set) * 1, &status, advertise_cp);
 
 	ret = hci_send_req(device, &enable_adv_rq, 1000);
-	if ( ret < 0 ) {
+	if (ret < 0)
+	{
 		hci_close_dev(device);
 		perror("Failed to enable advertising.");
 		return 0;
 	}
+	printf("enable adv data...\n");
+
+	struct timeval time;
+	char str[30];
+	while (!app_stopped)
+	{
+		gettimeofday(&time, NULL);
+		sprintf(str, "%ld.%ld", time.tv_sec, (time.tv_sec * 1000 + time.tv_usec / 1000));
+		memcpy(adv_data_cp->data, str, sizeof(str));
+		hci_send_req(device, &adv_data_rq, 1000);
+		printf("update: %s\n", str);
+	}
+
+	advertise_cp->enable = 0x00;
+	struct hci_request disable_adv_rq = ble_hci_request(
+		BT_HCI_CMD_LE_SET_EXT_ADV_ENABLE,
+		sizeof(struct bt_hci_cmd_le_set_ext_adv_enable), &status, advertise_cp);
+	ret = hci_send_req(device, &disable_adv_rq, 1000);
+	if (ret < 0)
+	{
+		hci_close_dev(device);
+		perror("Failed to disable advertising.");
+		return 0;
+	}
+	printf("disable adv data...\n");
 
 	hci_close_dev(device);
-	
+
 	return 0;
 }
--- scan.c
+++ scan.c
@@ -12,8 +12,14 @@
 #include <bluetooth/bluetooth.h>
 #include <bluetooth/hci.h>
 #include <bluetooth/hci_lib.h>
+#include "bt.h"
+#include <signal.h>
 
-struct hci_request ble_hci_request(uint16_t ocf, int clen, void * status, void * cparam)
+bool app_stopped = false;
+bool force_exit = false;
+int device = -1;
+
+struct hci_request ble_hci_request(uint16_t ocf, int clen, void *status, void *cparam)
 {
 	struct hci_request rq;
 	memset(&rq, 0, sizeof(rq));
@@ -26,75 +32,99 @@ struct hci_request ble_hci_request(uint16_t ocf, int clen, void * status, void *
 	return rq;
 }
 
+void sigint_handler(int sig)
+{
+	if (sig == SIGINT)
+	{
+		// ctrl+c退出时执行的代码
+		printf("will exit\n");
+		app_stopped = true;
+		if (force_exit)
+		{
+			struct bt_hci_cmd_le_set_ext_scan_enable scan_cp;
+			// disable scanning.
+			memset(&scan_cp, 0, sizeof(scan_cp));
+			scan_cp.enable = 0x00; // disable flag.
+			int status;
+			struct hci_request disable_adv_rq = ble_hci_request(BT_HCI_CMD_LE_SET_EXT_SCAN_ENABLE, sizeof(scan_cp), &status, &scan_cp);
+			int ret = hci_send_req(device, &disable_adv_rq, 1000);
+			if (ret < 0)
+			{
+				hci_close_dev(device);
+				perror("failed to enable scan.");
+			}
+			exit(1);
+		}
+
+		force_exit = true;
+	}
+}
+
 int main()
 {
+	signal(SIGINT, sigint_handler);
 	int ret, status;
 
 	// Get HCI device.
 
-	const int device = hci_open_dev(hci_get_route(NULL));
-	if ( device < 0 ) { 
+	int device_id = hci_get_route(NULL);
+	// device_id = 0;
+	printf("device id: %d\n", device_id);
+	device = hci_open_dev(device_id);
+	if (device < 0)
+	{
 		perror("Failed to open HCI device.");
-		return 0; 
+		return 0;
 	}
 
 	// Set BLE scan parameters.
-	
-	le_set_scan_parameters_cp scan_params_cp;
-	memset(&scan_params_cp, 0, sizeof(scan_params_cp));
-	scan_params_cp.type 			= 0x00; 
-	scan_params_cp.interval 		= htobs(0x0010);
-	scan_params_cp.window 			= htobs(0x0010);
-	scan_params_cp.own_bdaddr_type 	= 0x00; // Public Device Address (default).
-	scan_params_cp.filter 			= 0x00; // Accept all.
 
-	struct hci_request scan_params_rq = ble_hci_request(OCF_LE_SET_SCAN_PARAMETERS, LE_SET_SCAN_PARAMETERS_CP_SIZE, &status, &scan_params_cp);
-	
+	struct bt_hci_cmd_le_set_ext_scan_params *scan_params_cp =
+		(struct bt_hci_cmd_le_set_ext_scan_params *)malloc(sizeof(struct bt_hci_cmd_le_set_ext_scan_params) + 10);
+	memset(scan_params_cp, 0, sizeof(struct bt_hci_cmd_le_set_ext_scan_params) + 10);
+	scan_params_cp->own_addr_type = 0x00;
+	scan_params_cp->filter_policy = 0x00;
+	scan_params_cp->num_phys = 0x01 | 0x04;
+	scan_params_cp->data[0] = 0x00;
+	*((uint16_t *)&scan_params_cp->data[1]) = htobs(0x28);
+	*((uint16_t *)&scan_params_cp->data[3]) = htobs(0x28);
+	scan_params_cp->data[5] = 0x00;
+	*((uint16_t *)&scan_params_cp->data[6]) = htobs(0x40);
+	*((uint16_t *)&scan_params_cp->data[8]) = htobs(0x28);
+
+	struct hci_request scan_params_rq = ble_hci_request(BT_HCI_CMD_LE_SET_EXT_SCAN_PARAMS, sizeof(struct bt_hci_cmd_le_set_ext_scan_params) + 10, &status, scan_params_cp);
+
 	ret = hci_send_req(device, &scan_params_rq, 1000);
-	if ( ret < 0 ) {
+	if (ret < 0)
+	{
 		hci_close_dev(device);
 		perror("Failed to set scan parameters data.");
 		return 0;
 	}
+	printf("set param OK\n");
 
-	// Set BLE events report mask.
-
-	le_set_event_mask_cp event_mask_cp;
-	memset(&event_mask_cp, 0, sizeof(le_set_event_mask_cp));
-	int i = 0;
-	for ( i = 0 ; i < 8 ; i++ ) event_mask_cp.mask[i] = 0xFF;
-
-	struct hci_request set_mask_rq = ble_hci_request(OCF_LE_SET_EVENT_MASK, LE_SET_EVENT_MASK_CP_SIZE, &status, &event_mask_cp);
-	ret = hci_send_req(device, &set_mask_rq, 1000);
-	if ( ret < 0 ) {
-		hci_close_dev(device);
-		perror("Failed to set event mask.");
-		return 0;
-	}
-
-	// Enable scanning.
-
-	le_set_scan_enable_cp scan_cp;
+	// LE Set Scan Enable
+	struct bt_hci_cmd_le_set_ext_scan_enable scan_cp;
 	memset(&scan_cp, 0, sizeof(scan_cp));
-	scan_cp.enable 		= 0x01;	// Enable flag.
-	scan_cp.filter_dup 	= 0x00; // Filtering disabled.
-
-	struct hci_request enable_adv_rq = ble_hci_request(OCF_LE_SET_SCAN_ENABLE, LE_SET_SCAN_ENABLE_CP_SIZE, &status, &scan_cp);
-
+	scan_cp.enable = 0x01;	   // Enable flag.
+	scan_cp.filter_dup = 0x00; // Filtering disabled.
+	struct hci_request enable_adv_rq = ble_hci_request(BT_HCI_CMD_LE_SET_EXT_SCAN_ENABLE, sizeof(scan_cp), &status, &scan_cp);
 	ret = hci_send_req(device, &enable_adv_rq, 1000);
-	if ( ret < 0 ) {
+	if (ret < 0)
+	{
 		hci_close_dev(device);
 		perror("Failed to enable scan.");
 		return 0;
 	}
+	printf("enable scan OK\n");
 
 	// Get Results.
-
 	struct hci_filter nf;
 	hci_filter_clear(&nf);
-	hci_filter_set_ptype(HCI_EVENT_PKT, &nf);
-	hci_filter_set_event(EVT_LE_META_EVENT, &nf);
-	if ( setsockopt(device, SOL_HCI, HCI_FILTER, &nf, sizeof(nf)) < 0 ) {
+	hci_filter_all_ptypes(&nf);
+	hci_filter_all_events(&nf);
+	if (setsockopt(device, SOL_HCI, HCI_FILTER, &nf, sizeof(nf)) < 0)
+	{
 		hci_close_dev(device);
 		perror("Could not set socket options\n");
 		return 0;
@@ -103,42 +133,49 @@ int main()
 	printf("Scanning....\n");
 
 	uint8_t buf[HCI_MAX_EVENT_SIZE];
-	evt_le_meta_event * meta_event;
-	le_advertising_info * info;
+	evt_le_meta_event *meta_event;
 	int len;
 
-	while ( 1 ) {
+	while (!app_stopped)
+	{
 		len = read(device, buf, sizeof(buf));
-		if ( len >= HCI_EVENT_HDR_SIZE ) {
-			meta_event = (evt_le_meta_event*)(buf+HCI_EVENT_HDR_SIZE+1);
-			if ( meta_event->subevent == EVT_LE_ADVERTISING_REPORT ) {
+		if (len >= HCI_EVENT_HDR_SIZE)
+		{
+			meta_event = (evt_le_meta_event *)(buf + HCI_EVENT_HDR_SIZE + 1);
+			if (meta_event->subevent == BT_HCI_EVT_LE_EXT_ADV_REPORT)
+			{
 				uint8_t reports_count = meta_event->data[0];
-				void * offset = meta_event->data + 1;
-				while ( reports_count-- ) {
-					info = (le_advertising_info *)offset;
-					char addr[18];
-					ba2str(&(info->bdaddr), addr);
-					printf("%s - RSSI %d\n", addr, (char)info->data[info->length]);
-					offset = info->data + info->length + 2;
+				void *offset = meta_event->data + 1;
+				while (reports_count--)
+				{
+					struct bt_hci_le_ext_adv_report *info = (struct bt_hci_le_ext_adv_report *)offset;
+					bdaddr_t *addr = (bdaddr_t *)info->addr;
+					bdaddr_t leaddr;
+					baswap(&leaddr, addr);
+
+					printf("%s - RSSI %d len: %d, data[%.*s]]\n", batostr(&leaddr),
+						   info->rssi, (int)info->data_len, info->data_len, info->data);
+
+					offset = info->data + info->data_len;
 				}
 			}
 		}
 	}
 
 	// Disable scanning.
-
 	memset(&scan_cp, 0, sizeof(scan_cp));
-	scan_cp.enable = 0x00;	// Disable flag.
-
-	struct hci_request disable_adv_rq = ble_hci_request(OCF_LE_SET_SCAN_ENABLE, LE_SET_SCAN_ENABLE_CP_SIZE, &status, &scan_cp);
+	scan_cp.enable = 0x00; // Disable flag.
+	struct hci_request disable_adv_rq = ble_hci_request(BT_HCI_CMD_LE_SET_EXT_SCAN_ENABLE, sizeof(scan_cp), &status, &scan_cp);
 	ret = hci_send_req(device, &disable_adv_rq, 1000);
-	if ( ret < 0 ) {
+	if (ret < 0)
+	{
 		hci_close_dev(device);
-		perror("Failed to disable scan.");
+		perror("Failed to enable scan.");
 		return 0;
 	}
 
 	hci_close_dev(device);
-	
+	printf("close ok!!!\n");
+
 	return 0;
 }
